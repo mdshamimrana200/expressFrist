f280b3c6c8f815d1f0e041fabebb5c7723ff5956
mdshamimrana200 <mdshamimrana20@gmail.com>
2024-03-28 19:41:19 +0600

    updateAll

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Shamim tumi succes")
+    res.send("<h1>Shamim tumi succes for vercel")
 })
 
 app.use("/users",routers)
